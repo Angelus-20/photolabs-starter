2021bdee1b1b66bd769f72127fe9fb98ee4c7783
edwin penuela <edwinalexander20@gmail.com>
2023-09-07 21:42:10 -0400

    using mock files for photo and topics

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,28 +2,30 @@ import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "mocks/topics";
+
+// const sampleDataForTopicList = [
+//   {
+//     id: "1",
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: "2",
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: "3",
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topic) => {
+      {topics.map((topic) => {
         return <TopicListItem label={topic.title} key={topic.id} />
       } )} 
     </div>
