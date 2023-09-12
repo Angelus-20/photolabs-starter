4bed705b2134610e5d1ff3744b3d0aeead9f2bd5
edwin penuela <edwinalexander20@gmail.com>
2023-09-12 19:40:58 -0400

    implement backend topics and photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.scss';
-import React, { useState } from 'react';
-
+import React, { useState,useEffect } from 'react';
 import HomeRoute from 'routes/HomeRoute';
+import { useApplicationData } from 'hooks/useApplicationData'; // Import your custom hook
 
 const App = () => {
+  // Use your custom hook to get the application data
+  const { photoData, topicData, fetchTopicData } = useApplicationData();
+
+  useEffect(() => {
+    fetchTopicData();
+  }, [fetchTopicData]);
 
   return (
     <div>
-      <HomeRoute/>
+      {/* Pass both photoData and topicData to HomeRoute as props */}
+      <HomeRoute photoData={photoData} topicData={topicData} />
     </div>
   );
 };
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -13,6 +13,13 @@ const reducer = (state, action) => {
       return { ...state, selectedImage: action.image, isModalOpen: true };
     case 'CLOSE_MODAL':
       return { ...state, selectedImage: null, isModalOpen: false };
+
+    case 'SET_PHOTO_DATA':
+      return { ...state, photoData: action.data };
+
+    case 'SET_TOPIC_DATA':
+      return { ...state, topicData: action.data };
+
     default:
       return state;
   }
@@ -23,7 +30,47 @@ export function useApplicationData() {
     favorites: [],
     isModalOpen: false,
     selectedImage: null,
+    photoData: [], // Initialize with an empty array for photo data
   });
+
+  useEffect(() => {
+    const apiUrl = '/api/photos';
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Dispatch the SET_PHOTO_DATA action with the data as payload
+        // console.log(data);
+        dispatch({ type: 'SET_PHOTO_DATA', data });
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }, []); // The empty dependency array ensures the effect runs only once when the component mounts
+
+  const fetchTopicData = () => {
+    const apiUrl = '/api/topics';
+  
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Dispatch the SET_TOPIC_DATA action with the data as payload
+        dispatch({ type: 'SET_TOPIC_DATA', data });
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  };
 
   const addToFavorites = (id) => {
     dispatch({ type: 'TOGGLE_FAVORITE', id });
@@ -37,6 +84,5 @@ export function useApplicationData() {
     dispatch({ type: 'CLOSE_MODAL' });
   };
 
-  return { ...state, addToFavorites, openModal, closeModal };
+  return { ...state, addToFavorites, openModal, closeModal, fetchTopicData };
 }
-
